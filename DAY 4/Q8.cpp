85af76fc878d57e6544ed502b47952f63bd42cbc
sjaivardhan-dev <sjaivardhan215@gmail.com>
2025-11-27 11:54:48 +0530

    Add files via upload

---
+++ DAY 4/Q8.cpp
@@ -0,0 +1,13 @@
+/* Q8 (User Inputs, Operations & Output)
+Write a program to find and display the sum of the first n natural numbers. */
+
+#include <stdio.h>
+int main(){
+    int n, sum;
+    printf("Enter any natural number: ");
+    scanf("%d", &n);
+
+    sum = n* (n+1)/2;
+    printf("Sum of first %d natural numbers is: %d\n", n, sum);
+    return 0;
+}
